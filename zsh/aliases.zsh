ec37359ffc9b851ee3fbf1797090962d0fea0d47
Sam Chatfield <sam.c@omnea.co>
2026-04-17 13:39:59 +0100

    move aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,8 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+
+# Omnea
+alias yui="yarn update:all && yarn install:all"
+alias yltt="yarn lint && yarn typecheck && yarn test:unit"
+alias ytc="yarn typecheck"
