40a45261e24e1e6bf90c1b7fb0688c27a8c39589
Sam Chatfield <sam.c@omnea.co>
2026-04-24 19:26:46 +0100

    add killfe omnea alias

--- omnea/aliases.zsh
+++ omnea/aliases.zsh
@@ -1,3 +1,4 @@
 alias yui="yarn update:all && yarn install:all"
 alias yltt="yarn lint && yarn typecheck && yarn test:unit"
 alias ytc="yarn typecheck"
+alias killfe='kill $(lsof -t -i:3000) 2>/dev/null'
