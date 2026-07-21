02e105e3203efe380dc3f00b664668c4f5d16102
Sam Chatfield <sam.c@omnea.co>
2026-07-21 17:20:29 +0100

    pntc alias

--- omnea/aliases.zsh
+++ omnea/aliases.zsh
@@ -1,5 +1,6 @@
 alias yui="yarn update:all && yarn install:all"
 alias yltt="yarn lint && yarn typecheck && yarn test:unit"
 alias ytc="yarn typecheck"
+alias pntc="pnpm typecheck"
 alias killfe='kill $(lsof -t -i:3000) 2>/dev/null'
 alias pn="pnpm"
